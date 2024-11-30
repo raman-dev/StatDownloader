405b7651662c6487891bc798c896e66f627c2dc9
raman-dev <ramandeep.dev582@gmail.com>
2024-11-30 18:37:31 -0500

    Initial commit

---
+++ scripts/content.js
@@ -0,0 +1,4 @@
+console.log('Content script ran....');
+//we have access to the dom from the content script
+// let h1 = document.querySelector('h1');
+// h1.textContent = 'Title Change from Content Script';
