8cb79f03691b82fce56187b43b4997893317f66f
raman-dev <ramandeep.dev582@gmail.com>
2024-11-30 21:17:46 -0500

    meh

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,22 @@
-console.log('Content script ran....');
+const extensionConsoleTag = 'StatDownloader: '
+console.log(extensionConsoleTag,'Content script ran....');
 //we have access to the dom from the content script
-// let h1 = document.querySelector('h1');
-// h1.textContent = 'Title Change from Content Script';
+let playerNameH1 = document.querySelector("h1 span");
+let nameSplit = playerNameH1.textContent.split(" ");
+let playerKey = "";
+for (const part of nameSplit) {
+    if (!isNaN(part[0])){//returns false if number value or string
+        break;
+    }
+    playerKey += ' ' + part.toLowerCase();
+}
+console.log(extensionConsoleTag,playerKey);
+/*  
+    store in chrome 
+    basketball-ref:
+        available-table:
+            name: playerKey
+            table: data
+    
+    overwrite everytime
+*/
